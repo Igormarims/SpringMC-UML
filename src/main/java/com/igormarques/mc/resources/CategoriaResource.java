b03589e8def683ab06e3b4cf252ee966945f4e2f
Igor marques <igormarquespe@gmail.com>
2021-11-30 00:23:30 -0300

    testing domain first class

--- src/main/java/com/igormarques/mc/resources/CategoriaResource.java
+++ src/main/java/com/igormarques/mc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.igormarques.mc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.igormarques.mc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+	
+
 	@GetMapping
-	public String testeRequest() {
-		return "Rest está funcionando ";
+	public List<Categoria> testeRequest() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 	
